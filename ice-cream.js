3cf70e1eb9c0c2efbf7409e34db10370dedfc24a
Kurtis Grant <kurtis.b.grant@gmail.com>
2021-10-22 17:44:47 +0000

    Ice-cream array solution

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Add 'root beer' to array
+iceCreamFlavours.push("root beer")
+console.log(iceCreamFlavours)
+
+// Log first flavour
+console.log(iceCreamFlavours[0])
+
+// Log last flavour
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1])
+
+// Log number of flavours
+console.log(iceCreamFlavours.length)
