80729dfa0891b1aca59de17883401fad71b2e95a
Kurtis Grant <kurtis.b.grant@gmail.com>
2021-10-22 18:20:27 +0000

    lastIndexOf function exercise. Provides index of last array item which matches the query value

---
+++ lastIndexOf.js
@@ -0,0 +1,14 @@
+const lastIndexOf = function (arr, query) {
+  let last = -1
+  for (let i = 0; i < arr.length; i++) {
+    if (query === arr[i]) last = i
+  }
+  return last
+}
+
+
+console.log(lastIndexOf([0, 1, 4, 1, 2], 1), "=?", 3);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 2), "=?", 4);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 3), "=?", -1);
+console.log(lastIndexOf([5, 5, 5], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
